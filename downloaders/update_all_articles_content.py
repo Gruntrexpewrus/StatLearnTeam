733986c30f8568ac5a95df00a0f906caa0e65460
Marco Muscas <muscas.marco@gmail.com>
2020-05-07 19:39:16 +0200

    Added imports for parsers

--- downloaders/update_all_articles_content.py
+++ downloaders/update_all_articles_content.py
@@ -12,18 +12,23 @@ Created on Wed May  6 20:03:05 2020
 HERE WE WILL UPDATE EACH ARTICLE CONTENT 
 
 '''
-from newsparsers.parser_template import ParserTemplate
-from newsparsers.fanpage_parser import FanpageParser
+import pandas as pd
+
+from newsparsers.fanpage_parser import FanpageParser #www.fanpage.it
+from newsparsers.gazzetta_mezzogiorno import LaGazzettaDelMezzogiornoParser #www.lagazzettadelmezzogiorno.it
+from newsparsers.meridiano_news import IlMeridianoNewsParser #www.ilmeridianonews.it
+from newsparsers.blitz_quotidiano import BlitzParser # www.blitzquotidiano.it
+from newsparsers.strettoweb import StrettoWebParser # www.strettoweb.com
+from newsparsers.salernonotizie import SalernonotizieParser # www.salernonotizie.it
+from newsparser.meteoweb import MeteowebParser # www.meteoweb.eu
+from newsparser.ilfattoquotidiano import IlFattoQuotidianoParser # www.ilfattoquotidiano.it
+
 
 intopic_dataset_path = '/home/marco/workspace/git/StatLearnTeam/dataset/intopic_it_articles.csv'
 updated_datasets_root_path = '/home/marco/workspace/git/StatLearnTeam/dataset/updated_contents/'
 
+dataset = pd.read_csv(intopic_dataset_path, sep = ';')
 
 #f = FanpageParser('www.fanpage.it', dataset_path = intopic_dataset_path)
 #data = f.get_updated_dataset()
 #f.write_dataset_to_file(updated_datasets_root_path)
-
-f = ParserTemplate('www.ilfattoquotidiano.it', intopic_dataset_path)
-data = f.get_updated_dataset()
-f.write_dataset_to_file(updated_datasets_root_path)
-    
